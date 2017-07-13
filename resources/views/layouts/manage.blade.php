6e159d225724237acb28ecc6aaa3e4cbf2ae4e62
ignmunoz <ign.munoz.v@gmail.com>
2017-07-13 12:03:37 -0400

    Created manage dashboard, add roles feature to login

--- resources/views/layouts/manage.blade.php
+++ resources/views/layouts/manage.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>CMS Laravel - MANAGEMENT</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -16,8 +16,10 @@
 </head>
 <body>
     @include('_includes.nav.main')
-    <div id="app">
 
+    @include('_includes.nav.manage')
+
+    <div id="app">
         @yield('content')
     </div>
 
--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,14 @@
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('welcome');
 });
 
 Auth::routes();
+
+Route::prefix('manage')->middleware('role:superadministrator|administrator|editor|author|contributor')->group(function(){
+	Route::get('/', 'ManageController@index');
+	Route::get('/dashboard', 'ManageController@dashboard')->name('manage.dashboard');
+});
 
 Route::get('/home', 'HomeController@index')->name('home');
